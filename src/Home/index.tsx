396577bb661647715770aebe72a5c9ce911703bb
Felipe Pereira Pinto <ofelipepereira@gmail.com>
2021-06-06 20:00:12 -0300

    home - login layout

--- src/Home/index.tsx
+++ src/Home/index.tsx
@@ -1,35 +1,49 @@
 import React from 'react'
 
-import { Container } from './styles'
+import { Container, Form } from './styles'
 
 const Home: React.FC = () => {
+  function handleLogin() {
+    // To-do in the future
+    // Login
+    // Authenticate
+    // Save Credentials (local storage? cookies? JWT?)
+
+    // Change state from 'logged-off' to 'logged-on'
+    // App should re-render based on that
+  }
   return (
     <Container>
-      <h1>MARVEL</h1>
+      <Form>
+        <h1>MARVEL</h1>
 
-      <h2>Bem-vindo(a) de volta!</h2>
-      <span>Acesse sua conta:</span>
+        <h2>Bem-vindo(a) de volta!</h2>
+        <span className='login-comment'>Acesse sua conta:</span>
+        
+        <input type='text' id='user' placeholder='Usuário'/>
+        <input type='password' id='password' placeholder='Senha'/>
 
-      <label htmlFor="user">Usuário</label>
-      <input type="text" name="user"/>
+        <div className='horizontal-separator'>
+          <label>
+            <input type='checkbox' className='save-login'/>
+            Salvar login
+          </label>
 
-      <label htmlFor="password">Senha</label>
-      <input type="password" name="password"/>
-
-      <div>
-        <div>
-          <input type="radio" name="save-login" id="save-login" />
-          <label htmlFor="save-login">Salvar login</label>
+          <a href='#' className='forgotten-password'>Esqueci a senha</a>
         </div>
 
-        <a href="#">Esqueci a senha</a>
-      </div>
-
-      <button>Entrar</button>
-      
-      <span>Ainda não tem o login? </span>
-      <a href="#">Cadastre-se</a>
-
+        <button
+          className='login'
+          onClick={() => {alert('clicou')}}
+        >
+          Entrar
+        </button>
+        
+        <span className='register'>
+          Ainda não tem o login?{' '}
+          <a href='#'>Cadastre-se</a>
+        </span>
+      </Form>
     </Container>
   )
 }
--- src/Home/styles.ts
+++ src/Home/styles.ts
@@ -1,5 +1,106 @@
-import styled from 'styled-components';
+
+import styled from 'styled-components'
+
 
 export const Container = styled.div`
+  height: 100vh;
   color: white;
-`;
+
+  display: flex;
+`
+
+export const Form = styled.form`
+  width: 380px;
+  margin-left: 150px;
+  
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  /* align-items: center; */
+
+  h1 {
+    display: inline;
+    padding: 0 10px;
+    margin: 0 auto 45px;
+
+    background-color: red;
+
+    font-family: Marvel;
+    font-size: 6.25rem;
+    font-weight: normal;
+  }
+
+  h2 {
+  color: red;
+  font-size: 1.875rem;
+  font-weight: normal;
+  }
+
+  span.login-comment {
+    margin-bottom: 20px;
+
+    color: #84848D;
+    font-size: 1.25rem;
+    font-weight: 300;
+  }
+
+  input {
+    margin-bottom: 18px;
+    padding: 28px;
+    border-radius: 56px;
+    
+    color: black;
+    font-size: 1.375rem;
+    font-weight: 300;
+
+    &::placeholder {
+      color: #D1D1D6;
+    }
+  }
+
+  div.horizontal-separator {
+    margin-top: 12px;
+    padding: 0 1rem;
+    
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    font-size: 0.9375rem;
+    color: #84848D;
+
+    a {
+      display: block;
+
+      color: #84848D;
+      border-bottom: 1px solid red;
+    }
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  button.login {
+    height: 65px;
+    margin-top: 12px;
+    border: 0;
+    border-radius: 65px;
+
+    background-color: red;
+    color: white;
+    font-size: 1.75rem;
+  }
+
+  span.register {
+    margin-top: 30px;
+
+    text-align: center;
+    color: #84848D;
+    font-size: 1rem;
+
+    a {
+      color: red;
+    }
+  }
+`
